705fa3204bd83f358e877ffce71f51090d95a17f
Nassim <achnassim34@gmail.com>
2024-07-16 22:11:22 +0200

    Setup de base du component about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,31 @@
 import '../styles/About.sass';
 import aboutBackground from '../assets/about_background.jpg'
+import Chevron from '../assets/chevron.png'
 
 function About() {
     return (
         <main className="about">
-            <div className="about__container">
-                <img className="about__container--image" src={aboutBackground} alt="Image de fond de la section à propos"/>
+            <div className="about__imageContainer">
+                <img className="about__imageContainer--image" src={aboutBackground} alt="Image de fond de la section à propos"/>
+            </div>
+
+            <div className="aboutContainer">
+                <div className="aboutContainer__box">
+                    <h3 className="aboutContainer__box--title">Fiabilité</h3>
+                    <img style={{cursor: "pointer"}} src={Chevron} alt="Icone chevron"/>
+                </div>
+                <div className="aboutContainer__box">
+                    <h3 className="aboutContainer__box--title">Respect</h3>
+                    <img style={{cursor: "pointer"}} src={Chevron} alt="Icone chevron"/>
+                </div>
+                <div className="aboutContainer__box">
+                    <h3 className="aboutContainer__box--title">Service</h3>
+                    <img style={{cursor: "pointer"}} src={Chevron} alt="Icone chevron"/>
+                </div>
+                <div className="aboutContainer__box">
+                    <h3 className="aboutContainer__box--title">Sécurité</h3>
+                    <img style={{cursor: "pointer"}} src={Chevron} alt="Icone chevron" className="aboutContainer__box--icon"/>
+                </div>
             </div>
         </main>
     )
